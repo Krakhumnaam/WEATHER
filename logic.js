b56080900791a66eade7fbc0f8696faa6eef0d7f
Rachit Raj Niroula <160651882+Krakhumnaam@users.noreply.github.com>
2024-06-10 23:57:31 +0545

    changes

---
+++ logic.js
@@ -0,0 +1,28 @@
+  const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+  const apiKey="613ea0f65b83816785fb70e79b98c670"
+ async function city_search(place)
+{
+        //    let apiResponse = await fetch(url +"jhapa"+`&appid=613ea0f65b83816785fb70e79b98c670`)
+        let apiResponse= await fetch(url+ place +`&appid=${apiKey}`);
+         var result= await apiResponse.json(); 
+         console.log(result) 
+         document.querySelector(".city_name").innerHTML= place
+      document.querySelector(".humidity").innerHTML= "HUMIDITY:"+result.main.humidity+"%"
+      document.querySelector(".temp").innerHTML= "TEMPERATURE: "+ result.main.temp +"°C"
+      document.querySelector(".wind_speed").innerHTML = "WIND SPEED:"  + result.wind.speed + "km/hr"
+      if(result.weather[0].main == "Clouds")
+    {
+        document.querySelector(".weather_icon").src="C:\Users\DELL\Desktop\GIT\WEATHER\CLOUDS.png"
+    }
+           
+}
+document.getElementById('search_btn').onclick=()=>{
+    var searchedPlace= document.getElementById('search_city').value
+
+
+    city_search(searchedPlace);
+   
+
+ 
+
+}
